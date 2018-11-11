56705810fce5a5acedc5bdc4573851b9509d53a8
krmax44 <hi@krmax44.de>
2018-11-11 11:04:37 +0100

    add fonts, refactor scripts, fix minor bugs

--- js/script.js
+++ js/script.js
@@ -1,26 +1,39 @@
+const host = 'http://172.22.42.122:3000';
+
+const choices = document.querySelector('.choices');
+const nextQuestion = document.querySelector('#nextQuestion');
+const issues = document.querySelector('#issues ul');
+
 async function getStarter() {
 	try {
-		const request = await fetch(`http://172.22.42.122:3000/starters`);
+		const request = await fetch(`${host}/starters`);
 		const data = await request.json();
 
-		// Fragen
-		document.querySelector("#nextQuestion").innerHTML = '<h2>Fragen</h2><h3>Beschreibe dein Problem</h3><ul class="choices"></ul>';
+		choices.innerHTML = '';
 		data.forEach(element => {
-			document.querySelector(".choices").innerHTML += '<li><button onClick="ids.push(\'' + element._id + '\');send()" >' + element.text + '</button></li>';
+			const button = document.createElement('button');
+			button.setAttribute('data-id', element._id);
+			button.addEventListener('click', (e) => {
+				ids.push(e.target.getAttribute('data-id'));
+				send();
+			});
+			button.innerHTML = element.text;
+			const li = document.createElement('li');
+			li.appendChild(button);
+			choices.appendChild(li);
 		});
-	} catch (e) {
+	}
+	catch (e) {
 		console.error('fetch error', e);
 	}
 }
 
-var ids = []
+const ids = [];
 
 function auswerten() {
-	console.log("auswerten");
 	ids.push(...[...document.querySelectorAll("input")]
 		.filter(element => element.checked)
 		.map(element => element.getAttribute('name')));
-	console.log(ids)
 	send();
 }
 
@@ -38,22 +51,67 @@ async function send() {
 			}),
 		});
 		const data = await request.json();
-		console.log(data);
 
-		// Fragen
-		document.querySelector("#nextQuestion").innerHTML = '<h2>Fragen</h2><h3>Wähle zutreffendes aus</h3><ul class="choices"></ul>';
+		document.querySelector('#nextQuestion h3').innerHTML = 'Bitte wählen Sie zutreffendes aus.';
+		choices.innerHTML = '';
+
+		if (data.symptoms.length === 0) {
+			document.querySelector('#nextQuestion h3').innerHTML = 'Sie finden mögliche Ursachen und deren Lösungen rechts.';
+			return;
+		}
+
 		data.symptoms.forEach(element => {
-			document.querySelector(".choices").innerHTML += '<li><label class="container">' + element.text + '<input type="checkbox" name="' + element._id + '" /><span class="checkmark"></span></label></li>';
-		});
-		document.querySelector(".choices").innerHTML += '<li><button onClick="auswerten();">Abschicken</button></li>';
+			const li = document.createElement('li');
 
-		// Problemlösungen
-		document.querySelector("#issues").innerHTML = '<h2>Mögliche Lösungen</h2><ul id="issues-ul"></ul>';
-		data.bestIssues.forEach(element => {
-			document.querySelector("#issues-ul").innerHTML += '<li><h3>' + element.text + '</h3><p>' + element.fix + '</p></li> <hr /> ';
+			const label = document.createElement('label');
+			label.classList.add('container');
+
+			const input = document.createElement('input');
+			input.setAttribute('type', 'checkbox');
+			input.setAttribute('name', element._id);
+
+			const span = document.createElement('span');
+			span.classList.add('checkmark');
+
+			const text = document.createElement('span');
+			text.innerHTML = element.text;
+			
+			label.appendChild(text);
+			label.appendChild(input);
+			label.appendChild(span);
+			li.appendChild(label);
+			choices.appendChild(li);
 		});
 
-	} catch (e) {
+		const button = document.createElement('button');
+		button.addEventListener('click', auswerten);
+		button.innerHTML = 'Abschicken';
+
+		const li = document.createElement('li');
+		li.appendChild(button);
+		choices.appendChild(li);
+
+		data.bestIssues.forEach((element, i) => {
+			const li = document.createElement('li');
+			const h3 = document.createElement('h3');
+			h3.innerHTML = element.text;
+
+			const p = document.createElement('p');
+			p.innerHTML = element.fix;
+
+			li.appendChild(h3);
+			li.appendChild(p);
+
+			const hr = document.createElement('hr');
+
+			issues.appendChild(li);
+			if (i + 1 !== data.bestIssues.length) {
+				issues.appendChild(hr);
+			}
+		});
+
+	}
+	catch (e) {
 		console.error('fetch error', e);
 	}
 }
